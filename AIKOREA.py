d4d9ecb296d860f739e0e3651b2f85f8c1f6daad
ParkMini <parkteammini@gmail.com>
2023-09-04 12:09:33 +0900

    AI KOREA용 코드 리펙토링

--- AIKOREA.py
+++ AIKOREA.py
@@ -7,63 +7,49 @@ import qrcode
 
 app = Flask(__name__)
 
-
-@app.route("/", methods=["GET"])  # 기본 접속 시 이미지 업로드 화면 출력
+@app.route("/", methods=["GET"])
 def main():
     return render_template("./index.html")
 
-@app.route("/upload2Img", methods=["POST"])  # 이미지 업로드 (2컷)
+@app.route("/upload2Img", methods=["POST"])
 def upload2Img():
     lastUploadTime = str(dt.datetime.now()).replace(":", "").replace(" ", "_")
-    os.mkdir("./static/" + lastUploadTime)  # 폴더 생성
     lastUploadDir = os.path.join("static", lastUploadTime)
-    uploadImg = request.files.getlist("images[]")  # 받은 이미지 목록을 변수에 저장
+    os.mkdir(lastUploadDir)
+    
+    uploadImg = request.files.getlist("images[]")
     username = request.form.get('username')
-    i = 0
-    for f in uploadImg:
-        f.save(lastUploadDir + "/" +
-               secure_filename(str(i) + ".png"))  # 순서대로 이미지 저장
-        i += 1
+    
+    for idx, f in enumerate(uploadImg):
+        f.save(os.path.join(lastUploadDir, f"{idx}.png"))
+    
     image2Processing(lastUploadDir, username)
     createQR(lastUploadDir, lastUploadTime)
-    return render_template("showQR.html", imgSrc = lastUploadTime + "/QR.jpg", imgDownloadSrc=lastUploadTime + "/result.jpg")
+    
+    return render_template("showQR.html", imgSrc=f"{lastUploadTime}/QR.jpg", imgDownloadSrc=f"{lastUploadTime}/result.jpg")
 
-def image2Processing(lastUploadDir, username): # 인생네컷 (2컷)
-    # for i in range(2):
-    #     img = Image.open(lastUploadDir + "/" + str(i) + ".jpg")
-    #     # 이미지를 1100px, 710px으로 크기 수정
-    #     img_resize = img.resize((1100, 710), Image.LANCZOS)
-    #     img_resize.save(lastUploadDir + "/" + str(i) + "_resize.jpg")
-
-    # 이미지 합치기
-    imgs = []
-    for i in range(2):
-        imgs.append(Image.open(lastUploadDir + "/" + str(i) + ".png"))
+def image2Processing(lastUploadDir, username):
+    imgs = [Image.open(os.path.join(lastUploadDir, f"{i}.png")) for i in range(2)]
     bgImg = Image.open("./AI KOREA.png")
+    
     draw = ImageDraw.Draw(bgImg)
     font = ImageFont.truetype('./GmarketSansTTF/GmarketSansTTFMedium.ttf', size=200)
-    text_position = (250, 110)
-    text = username
-    draw.text(text_position, text, font=font, fill="white")
-    bgImg_size = bgImg.size
-    newImg = Image.new("RGB", (bgImg_size[0], bgImg_size[1]))
+    draw.text((250, 110), username, font=font, fill="white")
+    
+    newImg = Image.new("RGB", bgImg.size)
     newImg.paste(bgImg, (0, 0))
     newImg.paste(imgs[0], (100, 440))
     newImg.paste(imgs[1], (100, 1626))
-    newImg.save(lastUploadDir + "/" + "result.jpg")  # 합쳐진 이미지 저장
-    return True
+    newImg.save(os.path.join(lastUploadDir, "result.jpg"))
 
-def createQR(lastUploadDir, lastUploadTime): # QR코드 생성
-    qrImg = qrcode.make("http://localhost:712/view?key=" + lastUploadTime)
-    qrImg.save(lastUploadDir + "/" + "QR.jpg")
-    return True
+def createQR(lastUploadDir, lastUploadTime):
+    qrImg = qrcode.make(f"http://localhost:712/view?key={lastUploadTime}")
+    qrImg.save(os.path.join(lastUploadDir, "QR.jpg"))
 
-
-@app.route("/view", methods=["GET"]) # 인생네컷 보기
+@app.route("/view", methods=["GET"])
 def view():
     key = request.args.get("key")
-    return render_template("view.html", imgSrc = key + "/result.jpg")
-
+    return render_template("view.html", imgSrc=f"{key}/result.jpg")
 
 if __name__ == '__main__':
     app.jinja_env.auto_reload = True
